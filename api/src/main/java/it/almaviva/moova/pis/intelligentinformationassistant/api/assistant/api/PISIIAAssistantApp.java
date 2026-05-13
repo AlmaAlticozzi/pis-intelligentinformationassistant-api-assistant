49d1c8a0d043028f3fbafdc5aad487223b77f7d3
Alticozzi Matteo <m.alticozzi@almaviva.it>
2026-05-13 17:43:38 +0200

    🔧 config: Added env file and some fnd field to application.yaml

--- api/src/main/java/it/almaviva/moova/pis/intelligentinformationassistant/api/assistant/api/PISIIAAssistantApp.java
+++ api/src/main/java/it/almaviva/moova/pis/intelligentinformationassistant/api/assistant/api/PISIIAAssistantApp.java
@@ -1,17 +1,14 @@
 package it.almaviva.moova.pis.intelligentinformationassistant.api.assistant.api;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+import java.util.TimeZone;
+
+@QuarkusMain
 public class PISIIAAssistantApp {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 15; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        Quarkus.run(args);
     }
 }
